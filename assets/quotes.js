93bb144c8d98c021afc2d963ac4f4d2cf8a9150d
do99 <64813664+do99@users.noreply.github.com>
2020-05-23 20:45:57 +0900

    Update quotes.js

--- assets/quotes.js
+++ assets/quotes.js
@@ -1,4 +1,4 @@
 // prettier-ignore
 export default [
-    { author: "wkwk", quotes: "Mohon maaf lahir dan batin yaa aqilla :)" },
+    { author: "wkwk", quotes: "Mohon maaf lahir dan batin yaa aqilla :D" },
 ];
